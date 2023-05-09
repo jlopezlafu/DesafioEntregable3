1e54cc405180c4bd6d23c04279c725a722f9e9bd
Juan Manuel Lopez Lafuente <juanlopezlafuente@hotmail.com>
2023-05-08 23:38:54 -0300

    PreEntrega Save 2

--- src/app.js
+++ src/app.js
@@ -1,55 +1,20 @@
-const express = require('express')
-const app = express()
-const PORT = 3000;
-const ProductManager = require('./productManager');
-const { createFile, productsToSave, validateNumber } = require('./products');
-const path = "./src/products.txt";
-createFile(path);
-const myProductManager = new ProductManager(path);
-myProductManager.loadInitialProducts(productsToSave);
+const express = require("express");
+const app = express();
+const PORT = 8080
+const pdctRouter = require("./src/routes/products.routes.js");
+const cartRouter = require("./src/routes/cart.routes.js");
 
-app.get('/', (req, res) => {
-    res.send('Mi Tienda de Ropa - Bienvenidos')
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-
-app.get("/products", async (req, res) => {
-    try {
-        const products = await myProductManager.getProducts();
-        const limit = req.query.limit;
-        const isValidLimit = validateNumber(limit);
-        products
-            ? isValidLimit
-                ? res.json(products.slice(0, limit))
-                : res.json(products)
-            : res.json({ error: "Productos No encontrados" });
-    } catch (err) {
-        console.log("getProducts", err);
-    }
-});
-
-
-app.get("/products/:id", async (req, res) => {
-    try {
-        const id = req.params.id;
-        const isValidId = validateNumber(id);
-        if (!isValidId) {
-            res.json({ error: "id Invalido" });
-            return;
-        }
-        const product = await myProductManager.getProductById(req.params.id);
-        product
-            ? res.json(product)
-            : res.json({ error: "Id no encontrado" + req.params.id });
-    } catch (err) {
-        console.log("getProductById", err);
-    }
-});
+apiRouter.use("/products", pdctRouter)
+apiRouter.use("/carts", cartRouter)
 
 try {
     app.listen(PORT, () =>
         console.log(
-            `Server started on PORT ${PORT} at ${new Date().toLocaleString()}`
+            `🚀 Server started on PORT ${PORT} at ${new Date().toLocaleString()}`
         )
     );
 } catch (error) {
